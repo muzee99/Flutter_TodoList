6881f17ead9573862f8072900dc149c7e676ed1f
muzee99 <muzee99@pusan.ac.kr>
2021-11-25 23:37:05 +0900

    get text from inputField

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Todo List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Todo List'),
@@ -32,23 +23,81 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class TodoItem {
+  const TodoItem({required this.content});
+  final String content;
+}
+
+typedef ListChangeCallback = Function(TodoItem item, bool isDone);
+
+class TodoListItem extends StatelessWidget{
+
+  TodoListItem({
+    required this.item,
+    required this.isDone,
+    required this.onListChanged,
+  }) : super(key: ObjectKey(item));
+
+  final TodoItem item;
+  final bool isDone;
+  final ListChangeCallback onListChanged;
+
+  Color getColor(BuildContext context) {
+    return isDone ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? getTextStyle(BuildContext context) {
+    if(!isDone) return null;
+    return const TextStyle(color: Colors.black54, decoration: TextDecoration.lineThrough);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onListChanged(item, isDone);
+      },
+      leading: CircleAvatar(
+        backgroundColor: getColor(context),
+      ),
+      title: Text(
+        item.content, 
+        style: getTextStyle(context),
+      ),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({required this.items, Key?key}) : super(key:key);
+
+  final List<TodoItem> items;
+
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final _todoList = <TodoItem>{};
+
+  void handleListChanged(TodoItem item, bool isDone) {
+    setState(() {
+      if(!isDone) _todoList.add(item);
+      else _todoList.remove(item);
+    });
+  }
+}
+
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String data = "";
 
   void _incrementCounter() {
     setState(() {
@@ -63,16 +112,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -104,8 +145,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 decoration: InputDecoration(
                   labelText: 'To do',
                 ),
+                onSubmitted: (String str) {
+                  setState(() {
+                    data = str;
+                  });
+                },
               ),
-            )
+            ),
+            Text(data),
           ],
         ),
         
