a96ee32f9a4eef7dfbf5eccaf11de78025988043
muzee99 <muzee99@pusan.ac.kr>
2021-11-26 16:53:04 +0900

    list with statefulWidget

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -7,13 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Todo List',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
       home: const MyHomePage(title: 'Todo List'),
     );
@@ -29,86 +28,74 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class TodoItem {
-  const TodoItem({required this.content});
-  final String content;
-}
+// class TodoItem {
+//   const TodoItem({required this.content});
+//   final String content;
+// }
 
-typedef ListChangeCallback = Function(TodoItem item, bool isDone);
+// typedef ListChangeCallback = Function(TodoItem item, bool isDone);
 
-class TodoListItem extends StatelessWidget{
+// class TodoListItem extends StatelessWidget{
 
-  TodoListItem({
-    required this.item,
-    required this.isDone,
-    required this.onListChanged,
-  }) : super(key: ObjectKey(item));
+//   TodoListItem({
+//     required this.item,
+//     required this.isDone,
+//     required this.onListChanged,
+//   }) : super(key: ObjectKey(item));
 
-  final TodoItem item;
-  final bool isDone;
-  final ListChangeCallback onListChanged;
+//   final TodoItem item;
+//   final bool isDone;
+//   final ListChangeCallback onListChanged;
 
-  Color getColor(BuildContext context) {
-    return isDone ? Colors.black54 : Theme.of(context).primaryColor;
-  }
+//   Color getColor(BuildContext context) {
+//     return isDone ? Colors.black54 : Theme.of(context).primaryColor;
+//   }
 
-  TextStyle? getTextStyle(BuildContext context) {
-    if(!isDone) return null;
-    return const TextStyle(color: Colors.black54, decoration: TextDecoration.lineThrough);
-  }
+//   TextStyle? getTextStyle(BuildContext context) {
+//     if(!isDone) return null;
+//     return const TextStyle(color: Colors.black54, decoration: TextDecoration.lineThrough);
+//   }
   
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onListChanged(item, isDone);
-      },
-      leading: CircleAvatar(
-        backgroundColor: getColor(context),
-      ),
-      title: Text(
-        item.content, 
-        style: getTextStyle(context),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       onTap: () {
+//         onListChanged(item, isDone);
+//       },
+//       leading: CircleAvatar(
+//         backgroundColor: getColor(context),
+//       ),
+//       title: Text(
+//         item.content, 
+//         style: getTextStyle(context),
+//       ),
+//     );
+//   }
+// }
 
-class TodoList extends StatefulWidget {
-  const TodoList({required this.items, Key?key}) : super(key:key);
+// class TodoList extends StatefulWidget {
+//   const TodoList({required this.items, Key?key}) : super(key:key);
 
-  final List<TodoItem> items;
+//   final List<TodoItem> items;
 
-  @override
-  _TodoListState createState() => _TodoListState();
-}
+//   @override
+//   _TodoListState createState() => _TodoListState();
+// }
 
-class _TodoListState extends State<TodoList> {
-  final _todoList = <TodoItem>{};
+// class _TodoListState extends State<TodoList> {
+//   final _todoList = <TodoItem>{};
 
-  void handleListChanged(TodoItem item, bool isDone) {
-    setState(() {
-      if(!isDone) _todoList.add(item);
-      else _todoList.remove(item);
-    });
-  }
-}
+//   void handleListChanged(TodoItem item, bool isDone) {
+//     setState(() {
+//       if(!isDone) _todoList.add(item);
+//       else _todoList.remove(item);
+//     });
+//   }
+// }
 
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String data = "";
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -117,23 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -155,13 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(data),
           ],
         ),
-        
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
